2f08cb799407f43a18eaf4e2d66a13891a331ed3
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-12 21:11:13 +0000

    added new model and table items

--- db/migrate/20180312203244_create_category_ideas.rb
+++ db/migrate/20180312203244_create_category_ideas.rb
@@ -3,6 +3,7 @@ class CreateCategoryIdeas < ActiveRecord::Migration[5.1]
     create_table :category_ideas do |t|
       t.integer :category_id
       t.integer :idea_id
+      t.integer :item_id
     end
   end
 end
