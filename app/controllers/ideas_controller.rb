6b1d8f86f0ddf93fe6ea39f925800f0fa942ef52
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-13 01:14:29 +0000

    new idea controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,8 +2,24 @@ class IdeasController < ApplicationController
 
 
 
-get '/ideas' do
-  erb :'ideas/index'
-end
+  get '/ideas' do
+    erb :'ideas/index'
+  end
+
+  get '/ideas/new' do
+    if logged_in?
+      erb :'ideas/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/ideas' do
+    binding.pry
+    params[:category_ids].map do |id|
+      Category.find_by(id: id).items
+    end
+  end
+
 
 end
