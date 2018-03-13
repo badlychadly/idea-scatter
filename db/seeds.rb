840dc223b93ac80fe40f7402a641a2fa5026917c
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-13 17:49:35 +0000

    added seeds file, altered associations

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+
+
+User.create(username: "tom", email: "tom@gmail.com", password: "tomtom")
+User.create(username: "pam", email: "pam@gmail.com", password: "pampam")
+User.create(username: "amy", email: "amy@gmail.com", password: "amyamy")
