d68c433f0a9b8f5beed4f2419511cd8866165287
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-17 00:50:43 +0000

    added form for new categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,11 +5,26 @@ class CategoriesController < ApplicationController
     erb :'categories/index'
   end
 
+  get '/categories/new' do
+    if logged_in?
+      erb :'categories/new'
+    else
+      redirect '/loggin'
+    end
+  end
+
+  post '/categories' do
+    Category.find_or_create_by(params)
+    redirect '/categories'
+  end
+
 
   get '/categories/:id' do
     @category = Category.find_by(id: params[:id])
     erb :'categories/show'
   end
+
+
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,5 @@ class User < ActiveRecord::Base
   def slug
     self.username.downcase.gsub(" ", "-")
   end
-end
 
 end
