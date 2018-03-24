3c8e8b5004b5719c9257089ebcf3a8cc871bf436
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-24 04:33:34 +0000

    added validation for an idea to have category when created and updated but not after created

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,5 +3,10 @@ class Idea < ActiveRecord::Base
   belongs_to :category
   validates_presence_of :content
   validates :content, length: { minimum: 6 }, uniqueness: { case_sensitive: false }
+  validates :category, presence: true, on: [ :create, :update ],
+    unless: Proc.new { |c| Category.exists?(c.id) }
+
+
+
 
 end
