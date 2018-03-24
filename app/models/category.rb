32e79ba73d6d7f9ea38a56747557ef00ea402329
Chad Montoya <chadmontoya21@yahoo.com>
2018-03-24 03:20:10 +0000

    added uniqueness: { case_sensitive: false } validation to category and idea

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,17 @@
 class Category < ActiveRecord::Base
   has_many :ideas
   has_many :users, through: :ideas
+  before_validation :normalize_name, on: :create
   validates_presence_of :name
-  validates :name, length: { minimum: 3 }, uniqueness: true
+  validates :name, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+
+
+
+
+  private
+    def normalize_name
+      self.name = name.downcase.titleize
+    end
 
 
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,6 @@ class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   validates_presence_of :content
-  validates :content, length: { minimum: 6 }, uniqueness: true
+  validates :content, length: { minimum: 6 }, uniqueness: { case_sensitive: false }
 
 end
